e997223cf5fa990e36752ff24b37870493ea572d
teemukoukkari <teemu.koukkari@gmail.com>
2025-01-25 17:18:10 +0200

    Refactor login code & don't try to recreate user in db

--- backend/src/controllers/login.ts
+++ backend/src/controllers/login.ts
@@ -29,19 +29,16 @@ router.get('/', async (req: Request, res: Response) => {
     redirect_uri:GOOGLE_OAUTH_REDIRECT_URI,
     client_id: GOOGLE_CLIENT_ID,
     response_type: 'code',
-
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email',
     ].join(' '),
   };
 
+  // Returns the Google login page URL. After login, Google redirects to the /oauth endpoint.
   const queryString = new URLSearchParams(options).toString();
-
   const url = `${rootUrl}?${queryString.toString()}`;
-
-  res.send(url);
-  //palauttaa Googlen kirjautumissivun renderöivän url:n. Kun käyttäjä kirjautuu google lähettää tiedot /oauth endpointtiin
+  res.send(url); 
 });
 
 router.get('/oauth', async (req: Request, res: Response): Promise<any> => {
@@ -53,52 +50,51 @@ router.get('/oauth', async (req: Request, res: Response): Promise<any> => {
       return res.status(400).send('OAuth Error');
     }
 
-    if (code) {
-      // Vaihtaa Googlelta saadun koodin tokeneihin
-      const { id_token, access_token } = (
-        await axios.post<TokenResponse>('https://oauth2.googleapis.com/token', {
-          code: code,
-          client_id: GOOGLE_CLIENT_ID,
-          client_secret: GOOGLE_CLIENT_SECRET,
-          redirect_uri: GOOGLE_OAUTH_REDIRECT_URI,
-          grant_type: 'authorization_code',
-        })
-      ).data;
-
-      //hakee tokeneilla käyttäjätiedot
-      const googleUser = (
-        await axios.get<GoogleUser>(
-          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
-          {
-            headers: {
-              Authorization: `Bearer ${id_token}`,
-            },
-          }
-        )
-      ).data;
-
-      // luo käyttäjä olion ja asettaa sen keksiksi
-      const user = {
-        name: googleUser.name,
-        email: googleUser.email,
-        picture: googleUser.picture,
-      };
-      console.log(user);
-      res.cookie('user', JSON.stringify(user));
-      
-      try {
-        await User.create({ ...user, google_id: googleUser.id });
-      } catch {
-        (error: any) => {
-          console.log(error);
-        };
-      }
-      res.redirect('http://localhost:5173/profile');
-    } else {
+    if (!code) {
       res.status(400).send('No authorization code provided.');
     }
+
+    // Exchanges the code received from Google for tokens
+    const { id_token, access_token } = (
+      await axios.post<TokenResponse>('https://oauth2.googleapis.com/token', {
+        code: code,
+        client_id: GOOGLE_CLIENT_ID,
+        client_secret: GOOGLE_CLIENT_SECRET,
+        redirect_uri: GOOGLE_OAUTH_REDIRECT_URI,
+        grant_type: 'authorization_code',
+      })
+    ).data;
+
+    // Fetches user information (id, name, email, picture) with tokens
+    const googleUser = (
+      await axios.get<GoogleUser>(
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${id_token}`,
+          },
+        }
+      )
+    ).data;
+
+    // create a user object and set it as a cookie
+    const user = {
+      name: googleUser.name,
+      email: googleUser.email,
+      picture: googleUser.picture,
+    };
+    res.cookie('user', JSON.stringify(user));
+    
+    // Save / update user in the database
+    if(await User.findOne({ where: { google_id: googleUser.id } })) {
+      await User.update(user, { where: { google_id: googleUser.id } });
+    } else {
+      await User.create({ ...user, google_id: googleUser.id });
+    }
+
+    res.redirect('http://localhost:5173/profile');
   } catch (error) {
-    console.error('OAuth Error:', error);
+    console.error('OAUTH error:', error);
     res.status(500).send('Internal Server Error');
   }
 });
