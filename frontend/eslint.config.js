5f48ccb6fd729ed91c482665e74b51ce41964e3a
teemukoukkari <teemu.koukkari@gmail.com>
2025-01-27 19:34:54 +0200

    ci: configure eslint & prettier to frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,40 @@
-import js from '@eslint/js'
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  ...compat.extends("plugin:@typescript-eslint/strict", "eslint:recommended"),
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser,
+      }
+    },
+    rules: {  
+    },
+  }
+];
+
+/*import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,9 +43,9 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],*/
+   // files: ['**/*.{ts,tsx}'],
+    /*languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
@@ -27,4 +63,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+)*/
