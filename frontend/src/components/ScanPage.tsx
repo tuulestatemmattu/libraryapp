ab40c51cfdd87dfdc8236a35327480e73dd99be3
yogajones <95644662+yogajones@users.noreply.github.com>
2025-01-28 12:13:03 +0200

    feat: add BookForm to scan page

--- frontend/src/components/ScanPage.tsx
+++ frontend/src/components/ScanPage.tsx
@@ -1,10 +1,13 @@
-const ScanPage = () =>{
+import BookForm from './BookForm';
+
+const ScanPage = () => {
   return (
     <div>
       <h2>Scan Page</h2>
       <p>This is the scan page</p>
+      <BookForm onSubmit={console.log} />
     </div>
-  )
-}
+  );
+};
 
 export default ScanPage;
