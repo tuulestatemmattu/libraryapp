96c2f737000251bf1de8217db35985489253dd13
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2025-01-28 13:54:20 +0200

    style: linted the frontend

--- frontend/src/components/BookListItem.tsx
+++ frontend/src/components/BookListItem.tsx
@@ -1,21 +1,21 @@
 interface BookListItemProps {
-    book: {
-        id: number;
-        title: string;
-        author: string;
-        isbn: string;
-        description: string;
-        publish_year: string;        
-    }
+  book: {
+    id: number;
+    title: string;
+    author: string;
+    isbn: string;
+    description: string;
+    publish_year: string;
+  };
 }
 
-const BookListItem = ({book}: BookListItemProps) => {
-    return (
-        <div>
-            <p>Title: {book.title}</p>
-            <p>Author: {book.author}</p>
-        </div>
-    )
-}
+const BookListItem = ({ book }: BookListItemProps) => {
+  return (
+    <div>
+      <p>Title: {book.title}</p>
+      <p>Author: {book.author}</p>
+    </div>
+  );
+};
 
 export default BookListItem;
