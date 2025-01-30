41a6d94bea4ba3bc3e61fed4e9504e0644a00f27
yogajones <95644662+yogajones@users.noreply.github.com>
2025-01-30 11:14:43 +0200

    fix: returning 'res' caused the "No overload matches this call" error

--- backend/src/controllers/book.ts
+++ backend/src/controllers/book.ts
@@ -3,11 +3,10 @@ import Book from '../models/book';
 
 const bookRouter = express.Router();
 
-bookRouter.post('/', async (req, res) => { // 'No overload matches this call' error
+bookRouter.post('/', async (req, res) => {
   // TODO: handle errors and edge cases
   const { title, authors, isbn, description, publishedDate } = req.body;
 
-  // TODO: check the fields below to confirm they comply with Google Books API and the Book model
   try {
     const existingBook = await Book.findOne({ where: { isbn } });
 
@@ -17,7 +16,7 @@ bookRouter.post('/', async (req, res) => { // 'No overload matches this call' er
         { where: { isbn } }
       );
       const updatedBook = await Book.findOne({ where: { isbn } });
-      return res.status(200).send(updatedBook);
+      res.status(200).send(updatedBook);
     } else {
       const newBook = await Book.create({
         title,
@@ -26,7 +25,7 @@ bookRouter.post('/', async (req, res) => { // 'No overload matches this call' er
         description,
         publishedDate,
       });
-      return res.status(201).send(newBook);
+      res.status(201).send(newBook);
     }
   } catch (error) {
     console.error('Error saving book:', error);
