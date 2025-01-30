53707461852ed5e45d8b886cf3d57c35ddb15ec9
teemukoukkari <teemu.koukkari@gmail.com>
2025-01-30 17:14:50 +0200

    ci: configure backend testing using jest
    Signed-off-by: teemukoukkari <teemu.koukkari@gmail.com>

---
+++ backend/tests/ping.test.ts
@@ -0,0 +1,25 @@
+import supertest from 'supertest';
+import app from '../src/app';
+import { connectToDatabase, disconnectDatabase } from '../src/util/db';
+
+const api = supertest(app);
+
+beforeAll(async () => {
+    await connectToDatabase()
+})
+
+describe('GET /api/ping', () => {
+    it('should return 200 OK', async () => {
+        const response = await api.get('/api/ping');
+        expect(response.status).toBe(200);
+    });
+
+    it('should return "pong"', async () => {
+        const response = await api.get('/api/ping');
+        expect(response.text).toBe('pong');
+    });
+});
+
+afterAll(async () => {
+    await disconnectDatabase()
+});
