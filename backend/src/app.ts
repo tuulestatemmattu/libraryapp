d244b92e27cfb6b0e89440e1a284c5a858bd811d
teemukoukkari <teemu.koukkari@gmail.com>
2025-01-30 17:14:05 +0200

    refactor: extract app from index.ts to app.ts
    Signed-off-by: teemukoukkari <teemu.koukkari@gmail.com>

---
+++ backend/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+import { NODE_ENV } from './util/config';
+import loginRouter from './controllers/login';
+import isbnRouter from './controllers/isbn_api';
+import bookRouter from './controllers/books';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/login', loginRouter);
+app.use('/api/isbn', isbnRouter);
+app.use('/api/books', bookRouter);
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
+});
+
+if (NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+}
+
+export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-
-import { PORT, NODE_ENV } from './util/config';
+import { PORT } from './util/config';
 import { connectToDatabase } from './util/db';
-import loginRouter from './controllers/login';
-import isbnRouter from './controllers/isbn_api';
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use('/api/login', loginRouter);
-app.use('/api/isbn', isbnRouter);
-
-if (NODE_ENV == 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-}
+import app from './app';
 
 const start = async () => {
   await connectToDatabase();
