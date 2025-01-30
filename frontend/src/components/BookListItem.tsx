b878d75d9d9af9deb4bc4ff1e39c0ec9ea94bc01
teemukoukkari <teemu.koukkari@gmail.com>
2025-01-30 10:02:21 +0200

    feat: make the booklist box inline

--- frontend/src/components/BookListItem.tsx
+++ frontend/src/components/BookListItem.tsx
@@ -11,7 +11,7 @@ interface BookListItemProps {
 
 const BookListItem = ({ book }: BookListItemProps) => {
   return (
-    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px', display: 'inline-block' }}>
       <p>Title: {book.title}</p>
       <p>Author: {book.author}</p>
       <p>Year: {book.publish_year}</p>
