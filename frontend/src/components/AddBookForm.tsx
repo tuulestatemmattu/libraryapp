d3563058f569ede23dfa9a130543de8ca98a1aaf
Dodo <dodoloksttrom@gmail.com>
2025-02-04 15:57:42 +0200

    Add: added user notifications

--- frontend/src/components/AddBookForm.tsx
+++ frontend/src/components/AddBookForm.tsx
@@ -2,7 +2,7 @@ import React, { useState, SyntheticEvent } from 'react';
 import { BookInterface } from '../interfaces/Book';
 
 interface BookFormProps {
-  onSubmit: (book: BookInterface) => void;
+  onSubmit: (book: BookInterface) => Promise<{ data: BookInterface; status: number }>;
   initialValues: BookInterface | null;
 }
 
@@ -12,8 +12,9 @@ const AddBookForm: React.FC<BookFormProps> = ({ onSubmit, initialValues }) => {
   const [isbn, setIsbn] = useState(initialValues?.isbn || '');
   const [description, setDescription] = useState(initialValues?.description || '');
   const [publishedDate, setPublishedDate] = useState(initialValues?.publishedDate || '');
+  const [notification, setNotification] = useState<string | null>(null);
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
     const book: BookInterface = {
       title,
@@ -22,12 +23,24 @@ const AddBookForm: React.FC<BookFormProps> = ({ onSubmit, initialValues }) => {
       description,
       publishedDate,
     };
-    onSubmit(book);
-    setTitle('');
-    setAuthors('');
-    setIsbn('');
-    setDescription('');
-    setPublishedDate('');
+
+    try {
+      const response = await onSubmit(book);
+
+      if (response?.status === 201 || response?.status === 200) {
+        setNotification('New book added successfully!');
+        setTitle('');
+        setAuthors('');
+        setIsbn('');
+        setDescription('');
+        setPublishedDate('');
+      } else {
+        setNotification('Failed to adding the book. Please try again!');
+      }
+    } catch (error) {
+      setNotification('Error ocurred while adding a book');
+    }
+    setTimeout(() => setNotification(null), 3000);
   };
 
   const handleClear = () => {
@@ -39,47 +52,54 @@ const AddBookForm: React.FC<BookFormProps> = ({ onSubmit, initialValues }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="author">Author:</label>
-        <input
-          type="text"
-          id="author"
-          value={authors}
-          onChange={(e) => setAuthors(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="isbn">ISBN:</label>
-        <input type="text" id="isbn" value={isbn} onChange={(e) => setIsbn(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="description">Description:</label>
-        <input
-          type="text"
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="publishYear">Publish Year:</label>
-        <input
-          type="text"
-          id="publishYear"
-          value={publishedDate}
-          onChange={(e) => setPublishedDate(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-      <button type="button" onClick={handleClear}>
-        Clear
-      </button>
-    </form>
+    <div>
+      {notification && (
+        <div style={{ background: 'lightgreen', padding: '10px', marginBottom: '10px' }}>
+          {notification} {/* #TODO: move styling to css mui element for notif */}
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            value={authors}
+            onChange={(e) => setAuthors(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="isbn">ISBN:</label>
+          <input type="text" id="isbn" value={isbn} onChange={(e) => setIsbn(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="publishYear">Publish Year:</label>
+          <input
+            type="text"
+            id="publishYear"
+            value={publishedDate}
+            onChange={(e) => setPublishedDate(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <button type="button" onClick={handleClear}>
+          Clear
+        </button>
+      </form>
+    </div>
   );
 };
 
