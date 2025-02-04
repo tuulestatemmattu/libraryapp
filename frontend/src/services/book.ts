bf5d2e8c0e6984a3e0348dcaf6f7651193273a66
Dodo <dodoloksttrom@gmail.com>
2025-02-04 15:54:57 +0200

    Add: added status tracking for specific user notifications/errors

--- frontend/src/services/book.ts
+++ frontend/src/services/book.ts
@@ -3,9 +3,9 @@ import { apiBaseUrl } from '../constants';
 const baseUrl = apiBaseUrl + '/books';
 import { BookInterface } from '../interfaces/Book';
 
-const addBook = async (book: BookInterface): Promise<BookInterface> => {
-  const resultData = (await axios.post(baseUrl, book)).data;
-  return resultData;
+const addBook = async (book: BookInterface): Promise<{ data: BookInterface; status: number }> => {
+  const response = await axios.post(baseUrl, book);
+  return { data: response.data, status: response.status };
 };
 
 export default addBook;
