1554d81f91797bac617b1872c1ee55cba29bb47b
yogajones <95644662+yogajones@users.noreply.github.com>
2025-02-05 12:53:30 +0200

    refact: move req.body validation to middleware for /api/books route

---
+++ backend/src/util/validation.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import isIsbn from 'validator/lib/isISBN';
+
+const bookValidator = (req: Request, res: Response, next: NextFunction) => {
+    const { isbn, publishedDate } = req.body;
+
+    if (!isIsbn(isbn)) {
+        res.status(400).send({ message: 'Invalid ISBN' });
+        return;
+    }
+
+    if (!/^(19|20)\d{2}$/.test(publishedDate)) {
+        res.status(400).send({ message: 'Invalid year' });
+        return;
+    }
+
+    next();
+};
+
+export default bookValidator;
