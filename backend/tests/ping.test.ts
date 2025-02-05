a207d43cb304c9cf0883cf8a1fe917b4126ec50a
teemukoukkari <teemu.koukkari@gmail.com>
2025-02-05 17:18:59 +0200

    style: lint also tests

--- backend/tests/ping.test.ts
+++ backend/tests/ping.test.ts
@@ -6,21 +6,21 @@ import { connectToDatabase, disconnectDatabase } from '../src/util/db';
 const api = supertest(app);
 
 beforeAll(async () => {
-    await connectToDatabase()
-})
+  await connectToDatabase();
+});
 
 describe('GET /api/ping', () => {
-    it('should return 200 OK', async () => {
-        const response = await api.get('/api/ping');
-        expect(response.status).toBe(200);
-    });
+  it('should return 200 OK', async () => {
+    const response = await api.get('/api/ping');
+    expect(response.status).toBe(200);
+  });
 
-    it('should return "pong"', async () => {
-        const response = await api.get('/api/ping');
-        expect(response.text).toBe('pong');
-    });
+  it('should return "pong"', async () => {
+    const response = await api.get('/api/ping');
+    expect(response.text).toBe('pong');
+  });
 });
 
 afterAll(async () => {
-    await disconnectDatabase()
+  await disconnectDatabase();
 });
