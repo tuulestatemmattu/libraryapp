6e7bfc5b5927347529525d42f4535cab6e6fcc6c
EetuKilpi <eetu.kilpi@outlook.com>
2025-02-06 12:12:37 +0200

    refactor: Moved css to its own file

---
+++ frontend/src/components/BookList/BookList.tsx
@@ -0,0 +1,76 @@
+import './BookList.css';
+import BookListItem from '../BookListItem/BookListItem';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { apiBaseUrl } from '../../constants';
+import { Paper, TextField, MenuItem, Box, Grid2 } from '@mui/material';
+
+interface Book {
+  id: number;
+  title: string;
+  authors: string;
+  isbn: string;
+  description: string;
+  publishedDate: string;
+}
+
+const filterOptions: (keyof Book | 'all')[] = ['all', 'title', 'authors', 'publishedDate'];
+
+const BookList = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [filter, setFilter] = useState('');
+  const [filterType, setFilterType] = useState<keyof Book | 'all'>('all');
+
+  useEffect(() => {
+    axios.get(`${apiBaseUrl}/books`).then((res) => {
+      setBooks(res.data);
+    });
+  }, []);
+
+  const filteredBooks = books.filter((book) => {
+    if (filterType === 'all') {
+      return (
+        String(book.title).toLowerCase().includes(filter.toLowerCase()) ||
+        String(book.authors).toLowerCase().includes(filter.toLowerCase()) ||
+        String(book.publishedDate).toLowerCase().includes(filter.toLowerCase())
+      );
+    }
+
+    return String(book[filterType]).toLowerCase().includes(filter.toLowerCase());
+  });
+
+  return (
+    <>
+      <Paper elevation={4} className="paper-container">
+        <Box className="filter-box">
+          <TextField
+            select
+            label="Filter By"
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value as keyof Book | 'all')}
+            className="filter-select"
+          >
+            {filterOptions.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option === 'all' ? 'All Fields' : option.charAt(0).toUpperCase() + option.slice(1)}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            fullWidth
+            placeholder="Search"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+        </Box>
+      </Paper>
+      <Grid2 container spacing={2} wrap="wrap" className="grid-container">
+        {filteredBooks.map((book) => (
+            <BookListItem key={book.id} book={book} />
+        ))}
+      </Grid2>
+    </>
+  );
+};
+
+export default BookList;
---
+++ frontend/src/components/BookListItem/BookListItem.tsx
@@ -0,0 +1,41 @@
+import './BookListItem.css';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
+interface BookListItemProps {
+  book: {
+    id: number;
+    title: string;
+    authors: string;
+    isbn: string;
+    description: string;
+    publishedDate: string;
+  };
+}
+
+const BookListItem = ({ book }: BookListItemProps) => {
+  return (
+    <Card variant="outlined" className="book-card">
+      <CardActionArea className="book-card-action">
+        <CardMedia
+          component="img"
+          src="https://m.media-amazon.com/images/I/91VvijsCGIL._AC_UF894,1000_QL80_.jpg"
+          alt="image"
+        />
+        <CardContent className="book-card-content">
+          <Typography gutterBottom variant="h5" component="div" className="book-title">
+            {book.title}
+          </Typography>
+          <Typography gutterBottom variant="body2" component="div" className="book-authors">
+            {book.authors}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default BookListItem;
