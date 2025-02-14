ca062f64c1ecd5ebc47f4716c488c27ed631642f
Leevi Suotula <leevi.suotula@gmail.com>
2025-02-14 11:54:21 +0200

    featf: got sidecrolling list working ish

--- frontend/src/components/ItemsSlider/ItemsSlider.tsx
+++ frontend/src/components/ItemsSlider/ItemsSlider.tsx
@@ -1,9 +1,5 @@
-import { useState, ReactElement } from 'react';
-import React from 'react';
+import { ReactElement } from 'react';
 import './ItemsSlider.css';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { Container } from 'react-bootstrap';
 
 interface Props {
   title: string;
@@ -11,62 +7,17 @@ interface Props {
 }
 
 const ItemsSlider = ({ title, children }: Props) => {
-  const scrl = React.useRef() as React.MutableRefObject<HTMLInputElement>;
-  const [scrollX, setscrollX] = useState(0);
-  const [scrollEnd, setScrollEnd] = useState(false);
-
-  const slide = (shift: number) => {
-    const currentScrl = scrl.current;
-    if (!currentScrl) return;
-
-    currentScrl.scrollBy({
-      left: shift,
-      behavior: 'smooth',
-    });
-
-    currentScrl.scrollLeft += shift;
-    setscrollX((prev) => prev + shift);
-
-    if (Math.floor(currentScrl.scrollWidth - currentScrl.scrollLeft) <= currentScrl.offsetWidth) {
-      setScrollEnd(true);
-    } else {
-      setScrollEnd(false);
-    }
-  };
-
-  const scrollCheck = () => {
-    const currentScrl = scrl.current;
-    if (!currentScrl) return;
-
-    setscrollX(currentScrl.scrollLeft);
-    if (Math.floor(currentScrl.scrollWidth - currentScrl.scrollLeft) <= currentScrl.offsetWidth) {
-      setScrollEnd(true);
-    } else {
-      setScrollEnd(false);
-    }
-  };
-
   return (
-    <Container fluid className="my-3 py-3 item-slider-container">
-      <h4 className="px-3 mb-3 item-title">{title}</h4>
-      <div className="item-slider">
-        <div
-          onClick={() => slide(-100)}
-          className={`left-arrow-left ${scrollX < 1 ? 'is-disabled-hide' : ''}`}
-        >
-          <ArrowBackIosIcon />
-        </div>
-        <div ref={scrl} onScroll={scrollCheck} className="item-container">
-          {children}
-        </div>
-        <div
-          className={`right-arrow-right ${!scrollEnd ? '' : 'is-disabled-hide'}`}
-          onClick={() => slide(+100)}
-        >
-          <ArrowForwardIosIcon />
+    <div style={{ overflow: 'hidden' }}>
+      <div>
+        <h2>{title}</h2>
+        <div style={{ display: 'flex', flexDirection: 'row', overflow: 'auto' }}>
+          {children.map((child) => (
+            <div style={{ padding: 20 }}>{child}</div>
+          ))}
         </div>
       </div>
-    </Container>
+    </div>
   );
 };
 
