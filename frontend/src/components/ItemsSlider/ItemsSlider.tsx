25ce9914d31c327cedb01e2e95ef4601c565056c
Leevi Suotula <leevi.suotula@gmail.com>
2025-02-14 13:23:05 +0200

    feat: working ish item slider. Items not clickable

--- frontend/src/components/ItemsSlider/ItemsSlider.tsx
+++ frontend/src/components/ItemsSlider/ItemsSlider.tsx
@@ -1,4 +1,6 @@
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import './ItemsSlider.css';
 
 interface Props {
@@ -7,11 +9,65 @@ interface Props {
 }
 
 const ItemsSlider = ({ title, children }: Props) => {
+  const scrollref = useRef<HTMLDivElement | null>(null);
+  const scrollAmount = 400;
   return (
     <div style={{ overflow: 'hidden' }}>
-      <div>
-        <h2>{title}</h2>
-        <div style={{ display: 'flex', flexDirection: 'row', overflow: 'auto' }}>
+      <h2>{title}</h2>
+      <div style={{ position: 'relative', backgroundColor: 'red' }}>
+        <div
+          style={{
+            justifyContent: 'space-between',
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            zIndex: 1,
+          }}
+        >
+          <div
+            style={{ margin: 'auto 0' }}
+            onClick={() => {
+              if (scrollref.current) scrollref.current.scrollLeft -= scrollAmount;
+            }}
+          >
+            <KeyboardArrowLeftIcon
+              fontSize="large"
+              style={{
+                margin: 'auto 0',
+                backgroundColor: 'rgba(255, 255, 255, 1)',
+                borderRadius: '1000px',
+                padding: '3px',
+                zIndex: 1,
+              }}
+            />
+          </div>
+          <div
+            style={{ margin: 'auto 0' }}
+            onClick={() => {
+              if (scrollref.current) scrollref.current.scrollLeft += scrollAmount;
+            }}
+          >
+            <KeyboardArrowRightIcon
+              fontSize="large"
+              style={{
+                backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                borderRadius: '1000px',
+                padding: '3px',
+              }}
+            />
+          </div>
+        </div>
+        <div
+          ref={scrollref}
+          style={{
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'row',
+            overflowX: 'auto',
+            scrollBehavior: 'smooth',
+          }}
+        >
           {children.map((child) => (
             <div style={{ padding: 20 }}>{child}</div>
           ))}
