3157ec1936c9f10e5424198da93ff7b9df81ddac
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2025-02-24 14:03:08 +0200

    feat: update index model to support many-to-many relationship between books and tags

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,10 +1,17 @@
 import Book from './book';
 import User from './user';
+import Tag from './tag';
+import ConnectionBookTag from './connection_book_tag';
 
 User.hasMany(Book);
 Book.belongsTo(User);
 
+Book.belongsToMany(Tag, { through: ConnectionBookTag })
+Tag.belongsToMany(Book, { through: ConnectionBookTag })
+
 User.sync({ alter: true });
 Book.sync({ alter: true });
+Tag.sync({ alter: true });
+ConnectionBookTag.sync({ alter: true });
 
-export { Book, User };
+export { Book, User, Tag, ConnectionBookTag };
