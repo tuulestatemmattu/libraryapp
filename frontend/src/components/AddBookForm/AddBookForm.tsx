5dc36f53894d9ba1fde7417e68c9ced1417f0ee0
Leevi Suotula <leevi.suotula@gmail.com>
2025-02-25 18:36:36 +0200

    fix: Add book form takes default location from app location

--- frontend/src/components/AddBookForm/AddBookForm.tsx
+++ frontend/src/components/AddBookForm/AddBookForm.tsx
@@ -1,10 +1,11 @@
-import React, { useState, SyntheticEvent } from 'react';
+import React, { useState, useContext, SyntheticEvent } from 'react';
 import { CreatedBook } from '../../interfaces/Book';
 import StyledInput from '../StyledInput/StyledInput';
 import LocationSelect from '../LocationSelect/LocationSelect';
 import { TextField, Button } from '@mui/material';
 import '../StyledInput/StyledInput';
 import { useNotification } from '../../context/NotificationsProvider/NotificationProvider';
+import { LocationContext } from '../../context/LocationProvider/LocationProvider';
 
 import '../../style.css';
 import './AddBookForm.css';
@@ -15,12 +16,14 @@ interface BookFormProps {
 }
 
 const AddBookForm: React.FC<BookFormProps> = ({ onSubmit, initialValues }) => {
+  const [defaultLocation, _setLocation] = useContext(LocationContext);
+
   const [title, setTitle] = useState(initialValues?.title || '');
   const [authors, setAuthors] = useState(initialValues?.authors || '');
   const [isbn, setIsbn] = useState(initialValues?.isbn || '');
   const [description, setDescription] = useState(initialValues?.description || '');
   const [publishedDate, setPublishedDate] = useState(initialValues?.publishedDate || '');
-  const [location, setLocation] = useState(initialValues?.location || 'Helsinki');
+  const [location, setLocation] = useState(initialValues?.location || defaultLocation);
   const { showNotification } = useNotification();
 
   const handleSubmit = async (e: SyntheticEvent) => {
