1d6a3f1cdcc3c326c95fc6dcc769aba96819ffd9
yogajones <95644662+yogajones@users.noreply.github.com>
2025-02-26 11:17:37 +0200

    feat: add BorrowedBooks

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,17 +1,19 @@
 import Book from './book';
+import BorrowedBooks from './borrowed_books';
 import User from './user';
 
 User.hasMany(Book);
-Book.belongsTo(User);
+BorrowedBooks.belongsTo(User);
 
 const syncModels = async () => {
   await User.sync({ alter: true });
-  await Book.sync({ alter: true });
+  await BorrowedBooks.sync({ alter: true });
 };
 
 const resetTables = async () => {
   await Book.destroy({ where: {} });
   await User.destroy({ where: {} });
+  await BorrowedBooks.destroy({ where: {} });
 };
 
-export { syncModels, resetTables, Book, User };
+export { syncModels, resetTables, Book, User, BorrowedBooks };
