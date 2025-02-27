64410da085229aa15560b42600b52918bdd02549
Axel <axel.lindroos1@gmail.com>
2025-02-27 13:05:40 +0200

    fix: removed old googleoauth module and credentials from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId="121463685112-vf3f3jav0o3crtvgumd41a92gtlcg342.apps.googleusercontent.com">
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </GoogleOAuthProvider>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
