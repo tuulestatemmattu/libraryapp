5292b8f3180cf6839e28d3d9a3a0a8efbfebc3fc
EetuKilpi <eetu.kilpi@outlook.com>
2025-03-10 14:28:35 +0200

    feat: tag table in admin panel

--- frontend/src/components/AdminPage/tables/TagTable.tsx
+++ frontend/src/components/AdminPage/tables/TagTable.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import {
   DataGrid,
@@ -13,56 +12,27 @@ import {
   GridToolbarQuickFilter,
 } from '@mui/x-data-grid';
 
-import { useNotification } from '../../../context/NotificationsProvider/NotificationProvider';
 import useRequireAdmin from '../../../hooks/useRequireAdmin';
-import Profile from '../../../interfaces/Profile';
-import { getUsers, promoteUsers } from '../../../services/admin';
+import { FetchedTag } from '../../../interfaces/Tags';
+import { getTags } from '../../../services/tag';
 
-const UserTable = () => {
+const TagTable = () => {
   useRequireAdmin();
   const [rows, setRows] = useState([]);
   const [selected, setSelected] = useState<string[]>([]);
-  const { showNotification } = useNotification();
 
   useEffect(() => {
-    getUsers().then((result) =>
+    getTags().then((result) =>
       setRows(
-        result.map((u: Profile) => {
-          return { id: u.email, name: u.name, email: u.email, admin: u.admin };
+        result.map((t: FetchedTag) => {
+          return { id: t.name, name: t.name };
         }),
       ),
     );
   }, []);
 
-  const handlePromotion = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    console.log(selected);
-
-    for (const user of selected) {
-      try {
-        const result = await promoteUsers(user);
-        console.log(result);
-        showNotification('User promoted successfully!', 'success');
-      } catch {
-        showNotification(`Failed to promote ${user}`, 'error');
-      }
-    }
-    const result = await getUsers();
-    setRows(
-      result.map((u: Profile) => ({
-        id: u.email,
-        name: u.name,
-        email: u.email,
-        admin: u.admin,
-      })),
-    );
-    setSelected([]);
-  };
-
   const columns: GridColDef[] = [
     { field: 'name', headerName: 'Name', width: 200 },
-    { field: 'email', headerName: 'Email', width: 250 },
-    { field: 'admin', headerName: 'Admin', width: 130 },
     { ...GRID_CHECKBOX_SELECTION_COL_DEF },
   ];
 
@@ -75,7 +45,6 @@ const UserTable = () => {
         <GridToolbarColumnsButton />
         <GridToolbarDensitySelector slotProps={{ tooltip: { title: 'Change density' } }} />
         <Box sx={{ flexGrow: 1 }} />
-        <Button onClick={handlePromotion}>Promote Selected Users</Button>
       </GridToolbarContainer>
     );
   };
@@ -83,7 +52,7 @@ const UserTable = () => {
   return (
     <div>
       <Box sx={{ textAlign: 'center' }}>
-        <h1>Users</h1>
+        <h1>Tags</h1>
       </Box>
       <Paper sx={{ height: 400, width: '100%' }}>
         <DataGrid
@@ -104,4 +73,4 @@ const UserTable = () => {
   );
 };
 
-export default UserTable;
+export default TagTable;
