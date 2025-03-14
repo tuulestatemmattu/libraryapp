2ba8cafa2a89c8b115d29b9415dc002180301092
Axel <axel.lindroos1@gmail.com>
2025-03-14 14:14:01 +0200

    feat: add book from booklist(still broken)

--- frontend/src/components/AdminPage/tables/BookTable.tsx
+++ frontend/src/components/AdminPage/tables/BookTable.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Box, Paper, Tooltip } from '@mui/material';
+import { Box, Button, Paper, SelectChangeEvent, Stack, TextField, Tooltip } from '@mui/material';
 import {
   DataGrid,
   GridColDef,
@@ -10,11 +10,27 @@ import {
   GridToolbarQuickFilter,
 } from '@mui/x-data-grid';
 
+import useMainStore from '../../../hooks/useMainStore';
 import { FetchedBook } from '../../../interfaces/Book';
-import { getBooks } from '../../../services/book';
+import { FetchedTag } from '../../../interfaces/Tags';
+import { addBook, getBooks } from '../../../services/book';
+import LocationSelect from '../../LocationSelect/LocationSelect';
+import TagSelect from '../../TagSelect/TagSelect';
 
 const BookTable = () => {
-  const [rows, setRows] = useState([]);
+  const [rows, setRows] = useState<FetchedBook[]>([]);
+  const tags = useMainStore((state) => state.tags);
+  const [selectedTags, setSelectedTags] = useState<FetchedTag[]>([]);
+  const [newBook, setNewBook] = useState({
+    title: '',
+    authors: '',
+    isbn: '',
+    publishedDate: '',
+    description: '',
+    location: 'Helsinki',
+    copies: 1,
+    tags: selectedTags,
+  });
 
   useEffect(() => {
     getBooks().then((result) =>
@@ -34,6 +50,44 @@ const BookTable = () => {
       ),
     );
   }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewBook({ ...newBook, [name]: value });
+  };
+
+  const handleLocationChange = (location: string) => {
+    setNewBook({ ...newBook, location });
+  };
+
+  const handleTagSelection = (event: SelectChangeEvent<string[]>) => {
+    const {
+      target: { value },
+    } = event;
+
+    if (typeof value !== 'string') {
+      const tagsToSelect = tags.filter((tag) => value.includes(tag.name));
+
+      setSelectedTags(tagsToSelect);
+    }
+  };
+
+  const handleAddBook = () => {
+    console.log(newBook);
+    addBook(newBook).then((result) => {
+      setRows([...rows, result]);
+      setNewBook({
+        title: '',
+        authors: '',
+        isbn: '',
+        publishedDate: '',
+        description: '',
+        location: 'Helsinki',
+        copies: 0,
+        tags: selectedTags,
+      });
+    });
+  };
 
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 100 },
@@ -85,7 +139,7 @@ const BookTable = () => {
       </Box>
       <Paper sx={{ height: 'auto', width: '100%' }}>
         <DataGrid
-          rows={rows}
+          rows={[...rows, ...(newBook.title ? [{ ...newBook, id: '-' }] : [])]}
           columns={columns}
           initialState={{ pagination: { paginationModel } }}
           pageSizeOptions={[5, 10]}
@@ -94,6 +148,50 @@ const BookTable = () => {
             toolbar: CustomToolBar,
           }}
         />
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 1, mr: 1, ml: 1 }}>
+          <Stack direction="row" spacing={1} alignItems={'center'} mb={1}>
+            <TextField
+              label="Title"
+              name="title"
+              value={newBook.title}
+              onChange={handleInputChange}
+            />
+            <TextField
+              label="Authors"
+              name="authors"
+              value={newBook.authors}
+              onChange={handleInputChange}
+            />
+            <TextField label="ISBN" name="isbn" value={newBook.isbn} onChange={handleInputChange} />
+            <TextField
+              label="Published Date"
+              name="publishedDate"
+              value={newBook.publishedDate}
+              onChange={handleInputChange}
+            />
+            <TextField
+              label="Description"
+              name="description"
+              value={newBook.description}
+              onChange={handleInputChange}
+            />
+
+            <LocationSelect value={newBook.location} onChangeLocation={handleLocationChange} />
+            <TextField
+              label="Copies"
+              name="copies"
+              value={newBook.copies}
+              onChange={handleInputChange}
+            />
+            <Box sx={{ width: 200 }}>
+              <TagSelect tags={tags} selectedTags={selectedTags} onSelectTag={handleTagSelection} />
+            </Box>
+
+            <Button variant="contained" onClick={handleAddBook}>
+              Add Book
+            </Button>
+          </Stack>
+        </Box>
       </Paper>
     </div>
   );
