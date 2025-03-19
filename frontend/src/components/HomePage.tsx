a0b4861fb7d83ff89ce2354bd01058c682d86751
Leevi Suotula <leevi.suotula@gmail.com>
2025-03-19 12:05:32 +0200

    feat: added reserved books to my books. Modified the sorting

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -3,6 +3,7 @@ import { Stack } from '@mui/material';
 import useMainStore from '../hooks/useMainStore';
 import BookList from './BookList/BookList';
 import ScrollableList from './ScrollableList/ScrollableList';
+import { FetchedBook } from '../interfaces/Book';
 
 import '../style.css';
 
@@ -16,14 +17,12 @@ const HomePage = () => {
           <ScrollableList
             title="Your books"
             books={books
-              /* Filter my books then sort first books that youve had the longest */
-              .filter((book) => book.borrowedByMe)
-              .sort((b1, b2) =>
-                b1.lastBorrowedDate === b2.lastBorrowedDate
-                  ? 0
-                  : b1.lastBorrowedDate < b2.lastBorrowedDate
-                    ? -1
-                    : 1,
+              /* Filter my books then sort first books that are reserved and ready, then books that you have loaned then books, then other reserved books */
+              .filter((book: FetchedBook) => book.borrowedByMe || book.queuedByMe)
+              .sort((b1: FetchedBook, b2: FetchedBook) =>
+                b1.queueSize == 0 && b2.queueSize != 0 ? -1 :
+                  (b1.borrowedByMe === b2.borrowedByMe ? 0 : b1.borrowedByMe ? -1 : 1) ||
+                  new Date(b1.lastBorrowedDate).getTime() - new Date(b2.lastBorrowedDate).getTime()
               )}
           />
         </Stack>
