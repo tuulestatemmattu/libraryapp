fd926400242f7c02a586ea3730655274d67bffe9
Leevi Suotula <leevi.suotula@gmail.com>
2025-03-19 12:06:32 +0200

    fix: close bookoverview on modifying the reserve state of book

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,9 +1,9 @@
 import { Stack } from '@mui/material';
 
 import useMainStore from '../hooks/useMainStore';
+import { FetchedBook } from '../interfaces/Book';
 import BookList from './BookList/BookList';
 import ScrollableList from './ScrollableList/ScrollableList';
-import { FetchedBook } from '../interfaces/Book';
 
 import '../style.css';
 
@@ -20,9 +20,11 @@ const HomePage = () => {
               /* Filter my books then sort first books that are reserved and ready, then books that you have loaned then books, then other reserved books */
               .filter((book: FetchedBook) => book.borrowedByMe || book.queuedByMe)
               .sort((b1: FetchedBook, b2: FetchedBook) =>
-                b1.queueSize == 0 && b2.queueSize != 0 ? -1 :
-                  (b1.borrowedByMe === b2.borrowedByMe ? 0 : b1.borrowedByMe ? -1 : 1) ||
-                  new Date(b1.lastBorrowedDate).getTime() - new Date(b2.lastBorrowedDate).getTime()
+                b1.queueSize == 0 && b2.queueSize != 0
+                  ? -1
+                  : (b1.borrowedByMe === b2.borrowedByMe ? 0 : b1.borrowedByMe ? -1 : 1) ||
+                    new Date(b1.lastBorrowedDate).getTime() -
+                      new Date(b2.lastBorrowedDate).getTime(),
               )}
           />
         </Stack>
