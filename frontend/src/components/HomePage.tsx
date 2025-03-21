418206e166b5769c46de0e62d502c8e8326acdc2
teemukoukkari <teemu.koukkari@gmail.com>
2025-03-21 18:57:30 +0200

    refactor: reduce cognitive load of sorting a lot

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mui/material';
 
 import useMainStore from '../hooks/useMainStore';
-import { FetchedBook } from '../interfaces/Book';
+import { BookStatus, FetchedBook } from '../interfaces/Book';
 import BookList from './BookList/BookList';
 import ScrollableList from './ScrollableList/ScrollableList';
 
@@ -10,38 +10,31 @@ import '../style.css';
 const HomePage = () => {
   const books = useMainStore((state) => state.books);
 
+  const sortByOrder = (statusOrder: BookStatus[]) => {
+    return (b1: FetchedBook, b2: FetchedBook) => {
+      if (b1.status === b2.status) {
+        return b1.title.localeCompare(b2.title);
+      }
+      return statusOrder.indexOf(b1.status) - statusOrder.indexOf(b2.status);
+    };
+  };
+
+  const yourBooks = books
+    .filter((book: FetchedBook) => book.borrowedByMe || book.queuedByMe)
+    .sort(sortByOrder(['ready', 'late', 'borrowed', 'reserved']));
+
+  const allBooks = books.sort(
+    sortByOrder(['available', 'unavailable', 'reserved', 'borrowed', 'late', 'ready']),
+  );
+
   return (
     <article>
       <h2>
         <Stack>
-          <ScrollableList
-            title="Your books"
-            books={books
-              /* Filter my books then sort first books that are reserved and ready, then books that you have loaned then books, then other reserved books */
-              .filter((book: FetchedBook) => book.borrowedByMe || book.queuedByMe)
-              .sort((b1: FetchedBook, b2: FetchedBook) =>
-                b1.queueSize == 0 && b2.queueSize != 0
-                  ? -1
-                  : (b1.borrowedByMe === b2.borrowedByMe ? 0 : b1.borrowedByMe ? -1 : 1) ||
-                    new Date(b1.lastBorrowedDate).getTime() -
-                      new Date(b2.lastBorrowedDate).getTime(),
-              )}
-          />
+          <ScrollableList title="Your books" books={yourBooks} />
         </Stack>
       </h2>
-      <BookList
-        /* Sort books 'Available, not available, borrow by me' all categorys alphabetically */
-        books={books.sort(
-          (b1, b2) =>
-            (b1.copiesAvailable > 0 === b2.copiesAvailable > 0
-              ? 0
-              : b1.copiesAvailable > 0
-                ? -1
-                : 1) ||
-            (b1.borrowedByMe === b2.borrowedByMe ? 0 : b1.borrowedByMe ? 1 : -1) ||
-            (b1.title === b2.title ? 0 : b1.title < b2.title ? -1 : 1),
-        )}
-      />
+      <BookList books={allBooks} />
     </article>
   );
 };
