0830266e16e60f695c243f5101948f4030b57114
yogajones <95644662+yogajones@users.noreply.github.com>
2025-04-07 13:32:25 +0300

    style: make prettier

--- frontend/src/components/BookOverview/BottomRowButtons/BottomRowButtons.tsx
+++ frontend/src/components/BookOverview/BottomRowButtons/BottomRowButtons.tsx
@@ -29,57 +29,57 @@ const BottomRowButtons: React.FC<BottomRowButtonsProps> = ({
   return (
     <>
       {profile?.admin && (
-      <Button
-        variant="contained"
-        className="book-overview-action-button"
-        onClick={() => handleEdit(book.id)}
-      >
-        Edit
-      </Button>
+        <Button
+          variant="contained"
+          className="book-overview-action-button"
+          onClick={() => handleEdit(book.id)}
+        >
+          Edit
+        </Button>
       )}
       {book.status === 'borrowed' || book.status === 'late' ? (
-      <>
-        <Button
-        variant="contained"
-        className="book-overview-action-button"
-        onClick={() => handleReturn(book.id)}
-        >
-        Return
-        </Button>
-        <Button
-        variant="contained"
-        className="book-overview-action-button"
-        onClick={() => handleExtend(book.id)}
-        >
-        Extend
-        </Button>
-      </>
+        <>
+          <Button
+            variant="contained"
+            className="book-overview-action-button"
+            onClick={() => handleReturn(book.id)}
+          >
+            Return
+          </Button>
+          <Button
+            variant="contained"
+            className="book-overview-action-button"
+            onClick={() => handleExtend(book.id)}
+          >
+            Extend
+          </Button>
+        </>
       ) : book.status === 'available' || book.status === 'ready' ? (
-      <Button
-        variant="contained"
-        className="book-overview-action-button"
-        onClick={() => handleBorrow(book.id)}
-      >
-        Borrow
-      </Button>
+        <Button
+          variant="contained"
+          className="book-overview-action-button"
+          onClick={() => handleBorrow(book.id)}
+        >
+          Borrow
+        </Button>
       ) : book.status === 'unavailable' ? (
-      <Button
-        variant="contained"
-        className="book-overview-action-button"
-        onClick={() => handleAddToQueue(book.id)}
-      >
-        Reserve
-      </Button>
+        <Button
+          variant="contained"
+          className="book-overview-action-button"
+          onClick={() => handleAddToQueue(book.id)}
+        >
+          Reserve
+        </Button>
       ) : book.status === 'reserved' ? (
-      <Button
-        variant="contained"
-        className="book-overview-action-button"
-        onClick={() => handleRemoveFromQueue(book.id)}
-      >
-        Unreserve
-      </Button>
+        <Button
+          variant="contained"
+          className="book-overview-action-button"
+          onClick={() => handleRemoveFromQueue(book.id)}
+        >
+          Unreserve
+        </Button>
       ) : (
-      <> </>
+        <> </>
       )}
     </>
   );
