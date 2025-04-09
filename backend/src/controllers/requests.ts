f442f0d6a75080a5181d5738520f3ec42a28b61c
Ville Davidsson <ville.d@hotmail.com>
2025-04-09 12:02:44 +0300

    feat: backend endpoint for getting all book requests

--- backend/src/controllers/requests.ts
+++ backend/src/controllers/requests.ts
@@ -1,10 +1,26 @@
 import expresss from 'express';
 
+import { User } from '../models';
 import BookRequest from '../models/book_request';
 import { requireAdmin } from '../util/middleware/requireAdmin';
 import { requireLogin } from '../util/middleware/requireLogin';
 
 const router = expresss.Router();
+
+router.get('/', requireAdmin, async (req, res) => {
+  const data = await BookRequest.findAll({
+    attributes: ['id', 'title', 'author', 'isbn'],
+    include: [
+      {
+        model: User,
+        attributes: ['name', 'email'],
+      },
+    ],
+  });
+
+  const bookRequests = data.map((bookRequest) => bookRequest.toJSON());
+  res.json(bookRequests);
+});
 
 router.post('/', requireLogin, async (req, res) => {
   const userId = req.userId as string;
