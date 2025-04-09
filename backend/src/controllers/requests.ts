64f086a29aefafa2447efc449b0178b5f7a721e4
Ville Davidsson <ville.d@hotmail.com>
2025-04-09 11:25:59 +0300

    feat: backend endpoint for deleting book requests

--- backend/src/controllers/requests.ts
+++ backend/src/controllers/requests.ts
@@ -1,6 +1,7 @@
 import expresss from 'express';
 
 import BookRequest from '../models/book_request';
+import { requireAdmin } from '../util/middleware/requireAdmin';
 import { requireLogin } from '../util/middleware/requireLogin';
 
 const router = expresss.Router();
@@ -17,6 +18,23 @@ router.post('/', requireLogin, async (req, res) => {
       isbn,
     });
     res.status(201).send(bookRequest);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).send({ message: error.message });
+    }
+  }
+});
+
+router.delete('/:id', requireAdmin, async (req, res) => {
+  try {
+    const requestId = req.params.id;
+    const requestTodelete = await BookRequest.findByPk(requestId);
+    if (!requestTodelete) {
+      res.status(404).send({ message: 'Request not found' });
+      return;
+    }
+    await requestTodelete.destroy();
+    res.status(204).send();
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(500).send({ message: error.message });
