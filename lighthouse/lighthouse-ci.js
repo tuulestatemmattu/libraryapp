20ccb756a681957a8fe03834f3cdd58ac08d15cc
Leevi Suotula <leevi.suotula@gmail.com>
2025-04-11 15:39:15 +0300

    feat: A lighthouse script that gets on the page. Authorization still missing

---
+++ lighthouse/lighthouse-ci.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import lighthouse from 'lighthouse';
+import * as chromeLauncher from 'chrome-launcher';
+import puppeteer from 'puppeteer';
+
+const url = process.argv[2];
+const bearer = process.argv[3];
+
+const user = {
+    name: 'Test user',
+    email: 'test.user@example.com',
+    picture: 'sample_picture_url',
+    admin: true
+}
+
+const chrome = await chromeLauncher.launch(/*{chromeFlags: ['--headless']}*/);
+const resp = await fetch(`http://localhost:${chrome.port}/json/version`);
+const {webSocketDebuggerUrl} = await resp.json();
+const browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
+await browser.setCookie({name: 'profile', value: JSON.stringify(user), domain: "ohtu-library-staging-c43b89853868.herokuapp.com"}, {name: 'token', value: `bearer ${bearer}`, domain: "ohtu-library-staging-c43b89853868.herokuapp.com"})
+const page = await browser.newPage()
+
+
+const options = {logLevel: 'info', output: 'html', onlyCategories: ['accessibility']};
+const runnerResult = await lighthouse(url, options, undefined, page);
+
+// `.report` is the HTML report as a string
+const reportHtml = runnerResult.report;
+fs.writeFileSync('lhreport.html', reportHtml);
+
+// `.lhr` is the Lighthouse Result as a JS object
+console.log('Report is done for', runnerResult.lhr.finalDisplayedUrl);
+console.log('Accessibility score was', runnerResult.lhr.categories.accessibility.score * 100);
+// chrome.kill();
