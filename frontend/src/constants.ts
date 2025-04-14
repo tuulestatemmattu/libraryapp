8d6ed388feacf6480c16a5a7eb2d03fed3abaae5
Leevi Suotula <leevi.suotula@gmail.com>
2025-04-14 13:17:51 +0300

    feat: Staging and local testing location added

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,19 +1,44 @@
 export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3001/api';
-export const officeLocations: string[] = [
-  'Helsinki',
-  'Tampere',
-  'Copenhagen',
-  'Aarhus',
-  'Munich',
-  'Berlin',
-  'Oslo',
-  'Łódź',
-  'Malmö',
-  'Stockholm',
-  'Gothenburg',
-  'Amsterdam',
-  'Zurich',
-  'London',
-  'Southampton',
-  'Philadelphia',
-];
+let officeLocationsTemp: string[];
+if (!import.meta.env.PRODUCTION) {
+  officeLocationsTemp = [
+    'Helsinki',
+    'Tampere',
+    'Copenhagen',
+    'Aarhus',
+    'Munich',
+    'Berlin',
+    'Oslo',
+    'Łódź',
+    'Malmö',
+    'Stockholm',
+    'Gothenburg',
+    'Amsterdam',
+    'Zurich',
+    'London',
+    'Southampton',
+    'Philadelphia',
+    'testing',
+  ];
+} else {
+  officeLocationsTemp = [
+    'Helsinki',
+    'Tampere',
+    'Copenhagen',
+    'Aarhus',
+    'Munich',
+    'Berlin',
+    'Oslo',
+    'Łódź',
+    'Malmö',
+    'Stockholm',
+    'Gothenburg',
+    'Amsterdam',
+    'Zurich',
+    'London',
+    'Southampton',
+    'Philadelphia',
+  ];
+}
+
+export const officeLocations = officeLocationsTemp;
