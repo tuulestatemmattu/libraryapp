097ef84cc895528a5135a52635fb78cc96341a07
yogajones <95644662+yogajones@users.noreply.github.com>
2025-04-16 14:27:58 +0300

    refactor: DRY

--- frontend/src/components/BookCard/BookCard.tsx
+++ frontend/src/components/BookCard/BookCard.tsx
@@ -12,6 +12,7 @@ import Typography from '@mui/material/Typography';
 
 import { FetchedBook } from '../../interfaces/Book';
 import BookModal from '../BookModal/BookModal';
+import { getPlaceholderSVG } from '../../util/svgUtils';
 
 import './BookCard.css';
 
@@ -22,36 +23,6 @@ interface BookCardProps {
 
 const BookCard = ({ book, loading }: BookCardProps) => {
   const [open, setOpen] = useState(false);
-
-  const getPlaceholderSVG = (book: FetchedBook) => {
-    const firstLetter = book.title ? book.title.charAt(0).toUpperCase() : '?';
-
-    const generateColorFromISBN = (isbn: string) => {
-      let hash = 0;
-      for (let i = 0; i < isbn.length; i++) {
-        hash = isbn.charCodeAt(i) + ((hash << 5) - hash);
-      }
-      const hue = Math.abs(hash % 360);
-      return hue;
-    };
-
-    return `data:image/svg+xml;utf8,
-      <svg xmlns="http://www.w3.org/2000/svg" width="200" height="300" viewBox="0 0 200 300">
-        <rect width="100%" height="100%" fill="hsl(${generateColorFromISBN(book.isbn)}, 80%, 75%)"/>
-        <rect x="0" y="0" width="30" height="100%" fill="hsl(${generateColorFromISBN(book.isbn)}, 60%, 55%)"/>
-
-        <defs>
-          <filter id="textShadow">
-            <feDropShadow dx="0" dy="0" stdDeviation="5" flood-color="black" flood-opacity="0.5"/>
-          </filter>
-        </defs>
-
-
-        <text x="115" y="150" font-size="110" fill="white" font-family="Arial" font-weight="bold" text-anchor="middle" dominant-baseline="middle" filter="url(%23textShadow)">
-          ${firstLetter}
-        </text>
-      </svg>`;
-  };
 
   const getStatusChipLabel = (book: FetchedBook): string => {
     if (book.status === 'borrowed') {
